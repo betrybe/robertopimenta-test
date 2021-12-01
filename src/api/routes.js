b883f988e335bde643256127054197844d5c84f6
Roberto Ayres Pimenta <87720894+robertoApimenta@users.noreply.github.com>
2021-12-01 19:26:32 -0300

    Rota e controller users
    Criada a rota post para cadastro de usuários, linter gerou erro de complexidade 6

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const { body } = require('express-validator');
+const user = require('../controllers/users.controller');
 
 const routes = express.Router();
+
+routes.get('/users', user.index);
+routes.post('/users', [body('email').isEmail()], user.create);
 
 module.exports = routes;
--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,7 +1,26 @@
+const { validationResult } = require('express-validator');
+const users = require('../models/users');
+
 module.exports = {
     index(request, response) {
         response.json({
             mensagem: 'Controler do usuário',
         });
+    },
+    async create(request, response) {
+        const errors = validationResult(request);
+        const { name, email, password } = request.body;
+        if (!errors.isEmpty() || !name || !email || !password) {
+            return response.status(400).json({ message: 'Invalid entries. Try again.' });
+        }
+        let user = await users.findOne({ email });
+        if (!user) {
+            const role = 'user';
+            const data = { name, email, password, role };
+            await users.create(data);
+            user = { name, email, role };
+            return response.status(201).json({ user });
+        }
+        return response.status(409).json({ message: 'Email already registered' });
     },
 };
