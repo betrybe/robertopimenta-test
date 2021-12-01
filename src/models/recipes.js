a0d8def5d41714c86f109feb5b6e872de1faa015
Roberto Ayres Pimenta <87720894+robertoApimenta@users.noreply.github.com>
2021-12-01 18:32:36 -0300

    Model recipes
    Criação da model recipes.js

---
+++ src/models/recipes.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const DataSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    ingredients: {
+        type: String,
+        required: true,
+    },
+    preparation: {
+        type: String,
+        required: true,
+    },
+    userId: {
+        type: String,
+        required: false,
+    },
+    image: {
+        type: String,
+        required: false,
+    },
+});
+
+const recipes = mongoose.model('recipes', DataSchema);
+
+module.exports = recipes;
