b3be693c196fae6755ed60a925c9aed72e57a96b
Roberto Ayres Pimenta <87720894+robertoApimenta@users.noreply.github.com>
2021-12-01 18:29:17 -0300

    Model users
    Criação da model users

--- src/api/app.js
+++ src/api/app.js
@@ -1,7 +1,30 @@
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/Cookmaster', {
+  useUnifiedTopology: true,
+}, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Conectado ao DB');
+  }
+});
+
+app.use(cookieParser());
+app.use(express.json());
+app.use((request, response, next) => {
+  response.header('Access-Control-Allow-Origin', '*');
+  response.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  response.header('Access-Control-Allow-Headers', 'X-PINGOTHER, Content-Type, Authorization');
+  app.use(cors());
+  next();
+});
 
 app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
 
--- src/integration-tests/change.me.test.js
+++ src/integration-tests/change.me.test.js
@@ -1,30 +1,4 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const app = require('../api/app')
 
-chai.use(chaiHttp);
-chai.should();
-
-describe('Usuários - Endpoints', () => {
-    describe('POST /users', () => {
-        it ('Rota POST para novo usuário', done => {
-            const user = {
-                name: "Roberto Pimenta",
-                email: "robertopimenta@email.com",
-                password: "12345678"
-            };
-            chai.request(app)
-            .post('/users')
-            .send(user)
-            .end((err, response) => {
-                response.should.have.status(201);
-                response.body.should.be.a('object');
-                response.body.should.have.property('_id');
-                response.body.should.have.property('name').eq(user.name);
-                response.body.should.have.property('email').eq(user.email);
-                response.body.should.have.property('role').eq('user');
-                done();
-            });
-        });
-    });
+describe('', () => {
+    
 });
