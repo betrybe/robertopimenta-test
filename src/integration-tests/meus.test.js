8880b05369f207598d90364a582decd37560f07d
Roberto Ayres Pimenta <87720894+robertoApimenta@users.noreply.github.com>
2021-12-06 16:56:16 -0300

    Update meus.test.js

--- src/integration-tests/meus.test.js
+++ src/integration-tests/meus.test.js
@@ -20,5 +20,46 @@ describe('Receitas - Endpoints', () => {
                     done();
                 });
         });
+        it('Rota POST para cadastrar receita sem o campo INGREDIENTS', done => {
+            const recipe = {
+                name: "receita ovo frito",
+                preparation: "quebre o ovo, jogue na frigideira, adicione o sal"
+            };
+            chai.request(app)
+                .post('/recipes')
+                .send(recipe)
+                .end((err, response) => {
+                    response.should.have.status(400);
+                    done();
+                });
+        });
+        it('Rota POST para cadastrar receita sem o campo PREPARATION', done => {
+            const recipe = {
+                name: "receita ovo frito",
+                ingredients: "ovo e sal"
+            };
+            chai.request(app)
+                .post('/recipes')
+                .send(recipe)
+                .end((err, response) => {
+                    response.should.have.status(400);
+                    done();
+                });
+        });
+    });
+    describe('POST /users', () => {
+        it('Rota POST para cadastrar usuário sem o campo NAME', done => {
+            const user = {
+                "email": "robeeppppjsssjjjeh@email.com",
+                "password": "12345678"
+            };
+            chai.request(app)
+                .post('/users')
+                .send(user)
+                .end((err, response) => {
+                    response.should.have.status(400);
+                    done();
+                });
+        });
     });
 });
