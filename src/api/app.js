ae83e53ba80fda2fdcbcb067ccd6c2efef2505b3
Roberto Ayres Pimenta <87720894+robertoApimenta@users.noreply.github.com>
2021-12-06 17:31:35 -0300

    Update app.js

--- src/api/app.js
+++ src/api/app.js
@@ -3,7 +3,12 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const routes = require('./routes');
+const { body } = require('express-validator');
+const user = require('../controllers/users.controller');
+const recipes = require('../controllers/recipes.controller');
+const images = require('../controllers/images.controller');
+const login = require('../controllers/login.controller');
+const upload = require('../middlewares/upload');
 
 const app = express();
 
@@ -27,8 +32,6 @@ app.use((request, response, next) => {
   next();
 });
 
-app.use(routes);
-
 app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
 
 // Não remover esse end-point, ele é necessário para o avaliador
@@ -36,5 +39,20 @@ app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+
+app.get('/users', user.index);
+app.post('/users', [body('email').isEmail()], user.create);
+app.post('/users/admin', user.createAdmin);
+app.post('/login', [body('email').isEmail()], login.login);
+
+app.post('/recipes', recipes.create);
+app.get('/recipes', recipes.listagem);
+app.get('/recipes/:id', recipes.listarId);
+app.put('/recipes/:id', recipes.editarReceita);
+app.delete('/recipes/:id', recipes.deletarReceita);
+
+app.put('/recipes/:id/image/', upload.single('image'), recipes.imagemReceita);
+
+app.get('/images/:id', images.listarImagem);
 
 module.exports = app;
